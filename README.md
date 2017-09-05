7fbba778f5d5ce4a499646afee443cd46965c25f
Vitor Lima <vitor@ferreiradelima.com>
2017-09-05 10:32:45 -0300

    Added files

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 ## What this does
-Get the last record from variable "water\_level", prints it to the console, multiple the value by 2 and insert in a new variable called water_level_double.
+Whenever the analysis run, it send a notification for the account and for a dashboard.
 
 ## How to use on Tago
 Do your own modifications if you want.<br>
 Upload to Tago analysis, in the admin website.<br>
-Add the environment variable `device_token` with the device token of your choice.
 
 ## How to run the analysis from your machine  
 Make sure you have npm and node installed in your machine.<br>
-Add the environment variable `device_token` with the device token of your choice, to the analysis configuration, in the admin website.<br>
 Open the index.js, change `MY-ANALYSIS-TOKEN-HERE` line for your analysis token.<br>
 Opens the terminal and run:
 
--- index.js
+++ index.js
@@ -1,40 +1,22 @@
 'use strict';
 const Analysis = require('tago/analysis');
-const Utils    = require('tago/utils');
-const Device   = require('tago/device');
+const Services = require('tago/services');
 
 /**
- * The function presumes that you have a enviroment variable, that can be set in the admin website, in the Analysis configuration:
- * -> device_token: token
- * It's simple get the variable "water_level", prints it to the console, multiple the value by 2 and insert in a new variable called water_level_double.
+ * The main function used by Tago to run the script.
+ * It sends a notification to the account and another one linked to a dashboard.
  * @param  {object} context automatic received from Tago
  */
 function run_analysis(context) {
-    //Convert the environment variables into an object.
-    //You can do this by yourself, we just added an easily way.
-    const env_vars  = Utils.env_to_obj(context.environment);
+    const notification = new Services(context.token).notification;
 
-    const my_device = new Device(env_vars.device_token);
+    let title = "Example";
+    let message = "This is a account notification example";
+    notification.send(title, message).then(context.log).catch(context.log);
 
-    my_device.find({"variable":"water_level", "query":"last_item"}).then((result_array) => {
-        //Check if array isn't empty
-        if (!result_array[0]) return context.log("Empty Array");
-
-        //query:last_item always return only one value
-        const value = result_array[0].value;
-        const time = result_array[0].time; //you can format time later
-
-        //print to console
-        context.log(`The last record of the water_level is ${value}. It was inserted at ${time}`);
-
-        //Multiple the water_level value by 2 and insert in another variable
-        const obj_to_save = {
-            "variable": "water_level_double",
-            "value": value * 2,
-        };
-
-        my_device.insert(obj_to_save).then(context.log("Successfully Inserted")).catch(error => context.log("Error when inserting:", error));
-
-    }).catch(context.log); //just print error
+    title = "Dashboard Notification";
+    message = "This will send notification for the dashboard";
+    let ref_id = "593720f04a21870013379975"
+    notification.send(title, message, ref_id).then(context.log).catch(context.log);
 }
 module.exports = new Analysis(run_analysis, 'MY-ANALYSIS-TOKEN-HERE');
