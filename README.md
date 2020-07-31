97633b8076a1c39acbe51d8eee6a6b824a8099ac
Felipe Lima <felipe@ferreiradelima.com>
2020-07-31 18:08:26 -0300

    update sdk

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Sends a notification to the account or a dashboard or a bucket.
 Do your own modifications if you want.<br>
 Upload to Tago analysis, in the admin website.<br>
 Use the environment variables in the analysis if needed.<br>
-## How to run the analysis from your machine  
+## How to run the analysis from your machine
 Make sure you have npm and node installed in your machine.<br>
 Open the analysis.js, change `MY-ANALYSIS-TOKEN-HERE` line for your analysis token.<br>
 Open the terminal and run:
 `npm install`<br>
-`node .`
+`node analysis.js`
--- analysis.js
+++ analysis.js
@@ -1,21 +1,28 @@
-'use strict';
-const Analysis = require('tago/analysis');
-const Utils = require('tago/utils');
-const Services = require('tago/services');
+const { Analysis, Services, Utils } = require("@tago-io/sdk");
 
 /**
  * The main function used by Tago to run the script.
  * It sends a notification to the account and another one linked to a dashboard.
- * @param  {object} context automatic received from Tago
  */
-function sendNotification(context) {
-  const env_var = Utils.env_to_obj(context.environment);
-  const message = env_var.message;
-  const title = env_var.title;
-  const ref_id = env_var.dashboard_id || env_var.bucket_id || undefined;
+async function sendNotification(context) {
+  const env_var = Utils.envToJson(context.environment);
 
-  const Notification = new Services(context.token).Notification;
-  Notification.send(title, message, ref_id).then(context.log).catch(context.log);
+  const notification = new Services({ token: context.token }).Notification;
+
+  try {
+    const service_response = await notification.send({
+      message: env_var.message,
+      title: env_var.title,
+      ref_id: env_var.dashboard_id || env_var.bucket_id || undefined,
+    });
+
+    context.log(service_response);
+  } catch (error) {
+    context.log(error);
+  }
 }
 
-module.exports = new Analysis(sendNotification, 'MY-ANALYSIS-TOKEN-HERE');
+module.exports = new Analysis(sendNotification);
+
+// To run analysis on your machine (external)
+// module.exports = new Analysis(sendNotification, { token: "YOUR-TOKEN" });
